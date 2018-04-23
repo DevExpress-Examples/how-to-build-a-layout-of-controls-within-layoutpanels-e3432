24a6f48c3915e090880328f368db26ef9ff848e0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to build a layout of controls within LayoutPanels
+
+
+<p>This example shows how to arrange controls within LayoutPanels forming a custom layout.</p><p>In the example two LayoutPanels are created with controls arranged in a specific manner. Each control is wrapped into a LayoutControlItem object. The LayoutControlItem objects are combined together using LayoutGroup objects.</p><br />
+
+
+<br/>
+
+
---
+++ VB/LayoutPanel_Content_Ex/My Project/AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("LayoutPanel_Content_Ex")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("LayoutPanel_Content_Ex")>
+<Assembly: AssemblyCopyright("Copyright ©  2011")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cc63b359-11e5-4ec5-9893-84674cfac44b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Revision and Build Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
